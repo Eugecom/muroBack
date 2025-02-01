5d093aa7f5de1f942a40705d43a803e0c6482951
Eugecom <145773534+Eugecom@users.noreply.github.com>
2025-02-01 12:55:39 -0300

    Primer commit

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const commentsRoutes = require('./routes/commentsRoutes.js');
 
 const app = express();
 
 app.use(bodyParser.json());
 
+app.use(cors());
 app.use('/api', commentsRoutes);
 
 module.exports = { app }
